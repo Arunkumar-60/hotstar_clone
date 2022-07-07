3ed152a9053174dd980305bc699707cd178e6223
Arunkumar-60 <84034637+Arunkumar-60@users.noreply.github.com>
2022-07-07 15:59:32 +0530

    Add files via upload
    initial commit with all the required Video and images files,
    there are some errors in the javascript code!!

---
+++ hotstar.js
@@ -0,0 +1,97 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
+    }
+];
+
+const carousel=document.querySelector('.carousel');
+let slider=[];
+
+let slideIndex=0;
+
+const createslide = () => {
+    if(slideIndex>=movies.length){
+        slideIndex=0;
+    }
+
+    let slide= document.createElement('div');
+    var imgElement= document.createElement('img');
+    let content= document.createElement('div');
+    let h1= document.createElement('h1');
+    let p= document.createElement('p');
+
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+
+    imgElement.src=movies[slideindex].image;
+    slideIndex++
+
+    //setting up images
+    slide.className='slider';
+    slide.className='slide-content';
+    h1.className='movie-title';
+    p.className='movie-des';
+
+    slider.push(slide);
+
+    if(slide.length)
+    {
+        sliders[0].style.marginLeft = `calc(-${100 * (slider.length - 2)}% - ${
+            30 * (slider.length - 2)
+        }px)`;
+    }
+}
+
+for(let i=0; i<3;i++){
+    createslide();
+}
+
+setInterval(()=>{
+    createslide();
+}, 3000);
+
+//video cards
+
+const videoCards= [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item =>{
+    item.addEventListener("mouseover",()=>{
+        let video =item.children[1];
+        video.play()
+    });
+    item.addEventListener("mouseleave", ()=>{
+        let video =item.children[1];
+        video.pause();
+    });
+});
+
+
+//card sliders
+
